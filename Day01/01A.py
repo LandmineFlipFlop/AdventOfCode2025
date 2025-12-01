b021cb6d4be9f83afed38f9f46fd4e595d77957b
LandmineFlipFlop <elystanmay@gmail.com>
2025-11-30 21:07:38 -0800

    day 1 part 1

--- Day01/01A.py
+++ Day01/01A.py
@@ -1,14 +1,19 @@
 import sys, time, copy
 sys.path.append("../useful_functions.py")
 from useful_functions import *
-with open ('01_data/test_data', 'r') as casefile:
-    lines = casefile.read().splitlines()
-# with open ('01_data/full_data', 'r') as casefile:
+# with open ('01_data/test_data', 'r') as casefile:
 #     lines = casefile.read().splitlines()
+with open ('01_data/full_data', 'r') as casefile:
+    lines = casefile.read().splitlines()
 startTime = time.time()
 count = 0
 
-
+dialnum = 50
+for turn in lines:
+    dialnum += int(turn)
+    dialnum = dialnum%100
+    if dialnum == 0:
+        count += 1
 
 
 
