d75433cd8d961583a7e9c72c2a9cf6e375d17929
LandmineFlipFlop <elystanmay@gmail.com>
2025-12-06 23:26:44 -0800

    send help... it work with test data but dont for full  );

--- Day06/06B.py
+++ Day06/06B.py
@@ -8,30 +8,65 @@ with open ('06_data/test_data', 'r') as casefile:
 startTime = time.time()
 count = 0
 
+
 i = 0
 while i < len(lines):
-    line = lines[i].split(' ')
-    tally = ''
-    new = []
-    for num in line:
-        if num == '':
-            tally += '0'
-        elif num == '+' or num == '*':
-            pass
-        else:
-            tally += num
-            new.append(tally)
-            tally = ''
-        print(new)
-    line = new
-    lines[i] = line
+    lines[i] = list(lines[i])
     i += 1
 
-# data import
-print(lines)
 i = 0
+longest = 0
+while i < len(lines):
+    if len(lines[i]) > longest:
+        longest = len(lines[i])
+    i += 1
+# setup
 
-while i < len(lines[0]):
+i = 0
+while i < len(lines):
+    i2 = 0
+    while i2 < longest - len(lines[i]):
+        lines[i].append(' ')
+        i2 += 1
+    i += 1
+
+i = 0
+new = []
+temp = []
+while i < longest:
+    i2 = 0
+    tally = ''
+    while i2 < len(lines) - 1:
+        if lines[i2][i] != ' ':
+            tally += lines[i2][i]
+        i2 += 1
+    if tally == '':
+        new.append(temp)
+        temp = []
+    else:
+        temp.append(tally)
+    i += 1
+new.append(temp)
+
+operation = []
+for digit in lines[len(lines) - 1]:
+    if digit != ' ':
+        operation.append(digit)
+
+i = 0
+while i < len(new):
+    i2 = 0
+    if operation[i] == '+':
+        summ = 0
+    else:
+        summ = 1
+    while i2 < len(new[i]):
+        if operation[i] == '+':
+            summ += int(new[i][i2])
+        else:
+            summ *= int(new[i][i2])
+        i2 += 1
+    count += summ
     i += 1
 
 
