953fbe588ed65e428edda48a2688ecef1a191c42
LandmineFlipFlop <elystanmay@gmail.com>
2025-12-08 21:43:25 -0800

    part 2 is kicking my butt, why is it like this?!?! Im going to bed

---
+++ Day09/09B.py
@@ -0,0 +1,33 @@
+import sys, time, copy
+sys.path.append("../useful_functions.py")
+from useful_functions import *
+with open ('09_data/test_data', 'r') as casefile:
+    lines = casefile.read().splitlines()
+# with open ('09_data/full_data', 'r') as casefile:
+#     lines = casefile.read().splitlines()
+startTime = time.time()
+count = 0
+
+def isgreenline(pos1,pos2):
+    return False
+
+def allgreen(pos1,pos2):
+    return False
+
+
+i = 0
+while i < len(lines):
+    lines[i] = lines[i].split(',')
+    lines[i][0] = int(lines[i][0])
+    lines[i][1] = int(lines[i][1])
+    i += 1
+
+for coord1 in lines:
+    for coord2 in lines:
+        size =abs( (coord2[0] - coord1[0]) + 1) * abs( (coord2[1] - coord1[1]) + 1)
+        if  size > count and allgreen(coord1,coord2):
+            count = size
+
+
+print(green("count: " + str(count)))
+timerstop(startTime)
